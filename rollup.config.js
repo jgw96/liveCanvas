26453d4e864d285193072edb2f5003c64fb09a88
jgw96 <jgw9617@gmail.com>
2021-03-04 15:48:48 -0800

    chore(): missing copy

--- rollup.config.js
+++ rollup.config.js
@@ -23,7 +23,8 @@ export default {
         { src: 'index.prod.html', dest: 'dist/', rename: 'index.html' },
         { src: 'assets/**/*', dest: 'dist/assets/' },
         { src: 'src/global.css', dest: 'dist/'},
-        { src: 'manifest.json', dest: 'dist/'}
+        { src: 'manifest.json', dest: 'dist/'},
+        { src: 'fast-components.js', dest: 'dist/'}
       ]
     }),
     injectManifest({
